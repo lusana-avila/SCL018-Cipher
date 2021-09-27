5e7c8a936625adc4f7585692a185f470e069c6ab
Lusana Avila <89271117+lusana-avila@users.noreply.github.com>
2021-09-27 09:40:48 -0300

    SCL018

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,61 @@
 const cipher = {
-  // ...
-};
+
+  encode: function (numeroDesplazamiento,textoCifrar){
+    let text = "";
+    let ascii = "";
+    if (numeroDesplazamiento === null || numeroDesplazamiento === 0 ){
+      throw new TypeError;
+    }
+
+    for (let i = 0; i < textoCifrar.length; i++){
+    
+      ascii =  textoCifrar.charCodeAt(i);
+      let newascii = (ascii - 65 + numeroDesplazamiento) % 26 + 65;
+      let newletter = String.fromCharCode(newascii);
+      text += newletter;
+      /*console.log(ascii, newascii, newletter, numeroDesplazamiento);*/
+      
+    }
+  
+    /*console.log("encode")*/
+    return text;
+  },
+
+ decode: function (numeroDesplazamiento,textoCifrar){
+  let text = "";
+  let ascii = "";
+  if (numeroDesplazamiento === null || numeroDesplazamiento === 0 ){
+    throw new TypeError;
+  }
+  
+  for (let i = 0; i < textoCifrar.length; i++) {
+  
+    ascii =  textoCifrar.charCodeAt(i);
+    let newascii = (ascii - 90 - numeroDesplazamiento) % 26 + 90;
+    let newletter = String.fromCharCode(newascii);
+    text += newletter;
+    /*console.log(ascii, newascii, newletter);*/
+    
+  }
+  /*console.log("decode")*/
+  return text;
+}
+
+}
+
 
 export default cipher;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
